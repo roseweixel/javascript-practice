3aa0c94c31e50245b46c609f9360fc477858cb61
Rose Weixel <rose.weixel@gmail.com>
2014-12-21 20:58:40 -0500

    finish fizzBuzz

---
+++ eloquent-javascript/2-2.js
@@ -0,0 +1,11 @@
+// fizzBuzz
+
+function fizzBuzz(n){
+  if (n % 15 == 0){
+    return 'fizzbuzz';
+  } else if (n % 3 == 0){
+    return 'fizz';
+  } else if (n % 5 == 0){
+    return 'buzz';
+  }
+}
